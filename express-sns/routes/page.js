32dbfa8a573cf0a236a19fbebbdb0c447cb708bf
sirfcsn <sirfcsn@gmail.com>
2019-01-16 23:54:27 +0900

    feat: Add express-sns and Add app.js

---
+++ express-sns/routes/page.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/profile', (req, res) => {
+    res.render('profile', { title: '내 정보 - expressSns', user: null });
+});
+
+router.get('/join', (req, res) => {
+    res.render('join', {
+        title: '회원가입 - expressSns',
+        user: null,
+        joinError: req.flash('joinError'),
+    });
+});
+
+router.get('/', (req, res, next) => {
+    res.render('main', {
+        title: 'expressSns',
+        twits: [],
+        user: null,
+        loginError: req.flash('loginError'),
+    })
+});
+
+module.exports = router;
